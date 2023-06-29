1061cf65bea450e96109568bab001f726fb678d8
“soulsands” <407221377@qq.com>
2023-06-29 15:26:09 +0800

    default title from content

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
-const {req, res} = api;
+const { req, res } = api;
 if (req.method == "POST") {
     // try to create under a note with label - messageInbox, you can rename it .
     let messageInbox = api.getNoteWithLabel("messageInbox");
@@ -8,23 +8,28 @@ if (req.method == "POST") {
     }
 
     // text note
-    let {content} = req.body;
-    if (content) {
-        let {title, labelString = ""} = req.body;
-        if (!title) {
-            title = "from ios shor tcut";
-        }
+    let { content } = req.body;
+    if (content !== undefined) {
+        let { title, labelString = "" } = req.body;
 
         if (!content) {
             content = "";
+
+            if (!title) {
+                title = "from ios shortcut";
+            }
         } else {
+            if (!title) {
+                title = content.slice(0, 20).replace("\n", " ");
+            }
+
             // normalize \n from message
             content = content.split("\n").reduce((final, block) => {
                 return (final += `<p>${block}</p>`);
             }, "");
         }
 
-        const {note} = api.createNewNote({
+        const { note } = api.createNewNote({
             parentNoteId: messageInbox.noteId,
             title,
             content,
@@ -50,16 +55,32 @@ if (req.method == "POST") {
             note.setLabel("from ios shortcut");
         }
 
-        res.status(200).json({code: 200, msg: "success", params: req.body, result: note.getPojo()});
+        res.status(200).json({
+            code: 200,
+            msg: "success",
+            params: req.body,
+            result: note.getPojo(),
+        });
         // files
     } else {
         const multer = require("multer");
         const importRoute = require("../routes/api/import");
-        const uploadMiddleware = multer().single("upload");
+
+        const multerOptions = {
+            fileFilter: (req, file, cb) => {
+                // UTF-8 file names are not well decoded by multer/busboy, so we handle the conversion on our side.
+                // See https://github.com/expressjs/multer/pull/1102.
+                file.originalname = Buffer.from(
+                    file.originalname,
+                    "latin1"
+                ).toString("utf-8");
+                cb(null, true);
+            },
+        };
+        const uploadMiddleware = multer(multerOptions).single("upload");
 
         uploadMiddleware(req, res, () => {
-
-            req.body.taskId = api.randomString(10);;
+            req.body.taskId = api.randomString(10);
             req.body.last = "true";
             req.params.parentNoteId = messageInbox.noteId;
 
@@ -72,7 +93,7 @@ if (req.method == "POST") {
                 replaceUnderscoresWithSpaces: "true",
             };
 
-            req.body = {...req.body, ...options};
+            req.body = { ...req.body, ...options };
 
             importRoute
                 .importToBranch(req)
